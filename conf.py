618966fa32589c103691aa624b963c926e13de8f
Michael George <mdgeorge@cs.cornell.edu>
2022-01-19 10:10:32 -0500

    added rtd theme

--- conf.py
+++ conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'Certora Prover Documentation'
@@ -27,11 +26,14 @@ release = '0.0'
 
 # -- General configuration ---------------------------------------------------
 
+import sphinx_rtd_theme
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
     'myst_parser',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'old']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
