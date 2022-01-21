689211ca6cd3fd4ff0711bcb6befca6e87729f02
Michael George <mdgeorge@cs.cornell.edu>
2022-01-21 10:59:37 -0500

    folding and unfolding

--- conf.py
+++ conf.py
@@ -37,12 +37,12 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = []
+templates_path = ['templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'old']
+exclude_patterns = ['_build', 'templates', 'Thumbs.db', '.DS_Store', 'old']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -55,4 +55,10 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['static']
+
+# -- Added to include css file -----------------------------------------------
+
+def setup(app):
+    app.add_css_file('custom.css')
+
