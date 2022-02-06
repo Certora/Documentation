e93477fb4f515fb98c4415805b93b7d1934f2e5b
Michael George <mdgeorge@cs.cornell.edu>
2022-02-06 18:09:08 -0500

    exclude EVMVerified from build

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ templates_path = ['templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'templates', 'Thumbs.db', '.DS_Store', 'old', 'README.md']
+exclude_patterns = ['_build', 'templates', 'Thumbs.db', '.DS_Store', 'old', 'README.md', 'EVMVerifier']
 
 
 # -- Options for HTML output -------------------------------------------------
