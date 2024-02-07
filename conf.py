4b8f33b7e02d08196ad5b3acca0a47f74d3ffbbd
Shoham Shamir <shoham@certora.com>
2024-02-07 15:27:19 +0200

    added logo

--- conf.py
+++ conf.py
@@ -12,52 +12,57 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./util'))
+
+from docsinfra.sphinx_utils import TAGS, CVL2Lexer
+
+sys.path.insert(0, os.path.abspath("./util"))
+
 
 # -- Project information -----------------------------------------------------
 
-project = 'Certora Prover Documentation'
-copyright = '2022, Certora, Inc'
-author = 'Certora, Inc'
+project = "Certora Prover Documentation"
+copyright = "2022, Certora, Inc"
+author = "Certora, Inc"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0'
+release = "0.0"
 
 
 # -- General configuration ---------------------------------------------------
-
-import sphinx_rtd_theme
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
-    'sphinx_rtd_theme',
-    'sphinx.ext.todo',
+    "myst_parser",
+    "sphinx.ext.todo",
+    "sphinx_rtd_theme",
+    "docsinfra.sphinx_utils.codelink_extension",
+    "docsinfra.sphinx_utils.includecvl",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ["templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
-    '_build',
-    'templates',
-    'Thumbs.db',
-    '.DS_Store',
-    'old',
-    'README.md',
-    'EVMVerifier',
-    'docs/cvl/Test/*',
-    'docs/user-guide/multicontract/LiquidityPoolExample',
-    'docs/cvl/cvl2/CVL2Examples',
-    'docs/cvl/cvl2/cvl1',
-    'docs/cvl/cvl2/cvl2',
-    '.github'
-    ]
+    "_build",
+    "templates",
+    "Thumbs.db",
+    ".DS_Store",
+    "old",
+    "README.md",
+    "EVMVerifier",
+    "docs/cvl/Test/*",
+    "docs/user-guide/multicontract/LiquidityPoolExample",
+    "docs/cvl/cvl2/CVL2Examples",
+    "docs/cvl/cvl2/cvl1",
+    "docs/cvl/cvl2/cvl2",
+    ".github",
+    "Examples/*",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -65,21 +70,28 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ["static"]
+
+# The Certora logo
+html_logo = "static/logo.svg"
+
+
+# -- codelink_extension configuration ----------------------------------------
+code_path = "Examples/"
+link_to_github = True
+
 
 # -- Custom setup ------------------------------------------------------------
 
-todo_include_todos = True
+# Do not show todo list unless in dev build
+todo_include_todos = tags.has(TAGS.is_dev_build)  # noqa: F821
+
 
 def setup(sphinx):
-    sphinx.add_css_file('custom.css')
-    from highlight import CVLLexer
-    sphinx.add_lexer("cvl", CVLLexer)
-    from pygments_lexer_solidity import SolidityLexer
-    sphinx.add_lexer("solidity", SolidityLexer)
-
+    sphinx.add_css_file("custom.css")
+    sphinx.add_lexer("cvl", CVL2Lexer)
